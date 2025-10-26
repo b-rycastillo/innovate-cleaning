98ecf7e7db5a799b313931eafad67ce58fdb4507
Bryan Castillo Martel <bryancastillomartel@Mac.lan>
2025-10-26 11:03:20 -0400

    small improvements

--- innovate-cleaning/src/pages/Home.tsx
+++ innovate-cleaning/src/pages/Home.tsx
@@ -1,5 +1,5 @@
 import ServicesTable from "../components/ServicesTable";
-import Header from "../components/Header";
+import Header from "../components/Header/Header";
 import Footer from "../components/Footer/Footer";
 import '../App.css';
 
